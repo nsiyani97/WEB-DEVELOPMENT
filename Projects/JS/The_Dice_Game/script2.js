521481d8c36b35020e529861d940498cd59fdfda
Nikhil Siyani <168623119+nsiyani97@users.noreply.github.com>
2025-12-24 10:38:55 +0530

    .

--- Projects/JS/The_Dice_Game/script2.js
+++ Projects/JS/The_Dice_Game/script2.js
@@ -3,10 +3,10 @@ function myFunction() {
   let ps = Math.floor(Math.random() * 6) + 1;
 
   let img1 = document.querySelector(".img1");
-  img1.setAttribute("src", `images/dice${pf}.png`);
+  img1.setAttribute("src", `Images/dice${pf}.png`);
 
   let img2 = document.querySelector(".img2");
-  img2.setAttribute("src", `images/dice${ps}.png`);
+  img2.setAttribute("src", `Images/dice${ps}.png`);
 
   let h1 = document.querySelector("h1");
   h1.textContent = `${pf > ps ? "🏆Player 1 Win" : ""} ${ps > pf ? "Player 2 Win🏆" : ""} ${ps == pf ? "Draw" : ""}`;
