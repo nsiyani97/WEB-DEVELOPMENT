0cc421655c6d42ec4a9ddea24ea72f73fd3e027e
Nikhil Siyani <168623119+nsiyani97@users.noreply.github.com>
2025-12-25 21:32:47 +0530

    .

--- Projects/REACT/The_Keeper_App/vite.config.js
+++ Projects/REACT/The_Keeper_App/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Projects/REACT/The_Keeper_App/",
+  base: "/WEB-DEVELOPMENT/Projects/REACT/The_Keeper_App/",
 });
